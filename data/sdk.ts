7182dc4fe3d8957040028762e67f1a2b37f5a3ed
David Mihal <david@dmihal.com>
2021-12-20 23:54:26 +0000

    Use DB for cache

--- data/sdk.ts
+++ data/sdk.ts
@@ -1,9 +1,19 @@
 import { CryptoStatsSDK } from '@cryptostats/sdk'
 
 export function getSDK() {
-  return new CryptoStatsSDK({
-    ipfsGateway: 'https://ipfs.cryptostats.community',
+  const sdk = new CryptoStatsSDK({
+    mongoConnectionString: process.env.MONGO_CONNECTION_STRING,
+    redisConnectionString: process.env.REDIS_URL,
     moralisKey: process.env.MORALIS_KEY,
-    adapterListSubgraph: 'dmihal/cryptostats-adapter-registry-test',
   })
+
+  sdk.getList('fees').setCacheKeyResolver((_id: string, query: string, params: string[]) =>
+    query === 'oneDayTotalFees' ? params[0] : null
+  );
+
+  sdk.getList('fees-range').setCacheKeyResolver((_id: string, query: string, params: string[]) =>
+    query === 'dateRangeTotalFees' && params.length == 2 ? `${params[0]}-${params[1]}` : null
+  );
+
+  return sdk
 }
